87a5d46a6e70c55e030066325d49ec7944af1804
Matej Basista <matej.basista@hulman.sk>
2023-09-21 21:46:16 +0200

    pingpong implemented

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,15 +2,30 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int p[2];
-  
-  p[0] = getpid();
-  p[1] = fork();
-  pipe(p);
-  fprintf(0, "%d" ,getpid());
+  int to[2];
+  int from[2];
+
+  pipe(to);
+  pipe(from);
+
+  int pid = fork();
+
+  if (pid == 0)
+  {
+    char mess;
+    read(from[0], &mess, 1);
+    printf("%d: received ping\n", getpid());
+    write(to[1], "x", 1);
+  }
+  else
+  {
+    char mess;
+    write(from[1], "x", 1);
+    read(to[0], &mess, 1);
+    printf("%d: received pong\n", getpid());
+  }
 
   exit(0);
 }
